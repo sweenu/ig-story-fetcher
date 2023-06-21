186b64f4b2a87432e9ab6586e7cf9852d3a97e31
Bruno Inec <contact@sweenu.xyz>
2023-06-21 14:05:20 +0200

    Revert "build for each default system"
    This reverts commit 2b26e8b88a33679fe25adb2371f191a70fd32d02.

--- flake.nix
+++ flake.nix
@@ -1,47 +1,39 @@
 {
   inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
-  inputs.flake-utils.url = "github:numtide/flake-utils";
 
-  outputs = { self, nixpkgs, flake-utils }:
+  outputs = { self, nixpkgs }:
     let
-      localOverlay = final: prev: {
-        ig-story-fetcher = prev.poetry2nix.mkPoetryApplication {
-          projectDir = self;
-          python = prev.python311;
-          overrides = prev.poetry2nix.defaultPoetryOverrides.extend (self: super: {
-            instagrapi = super.instagrapi.overridePythonAttrs
-              (old: { buildInputs = (old.buildInputs or [ ]) ++ [ super.setuptools ]; });
-          });
-        };
-      };
-
-      pkgsForSystem = system: import nixpkgs {
-        inherit system;
-        overlays = [ localOverlay ];
-      };
+      pkgs = nixpkgs.legacyPackages.x86_64-linux;
+      p2n-overrides = pkgs.poetry2nix.defaultPoetryOverrides.extend (self: super: {
+        instagrapi = super.instagrapi.overridePythonAttrs
+          (old: { buildInputs = (old.buildInputs or [ ]) ++ [ super.setuptools ]; });
+      });
     in
-    flake-utils.lib.eachDefaultSystem
-      (system: rec
-      {
-        legacyPackages = pkgsForSystem system;
+    {
+      packages.x86_64-linux.ig-story-fetcher = pkgs.poetry2nix.mkPoetryApplication {
+        projectDir = self;
+        python = pkgs.python311;
+        overrides = p2n-overrides;
+      };
+      packages.x86_64-linux.default = self.packages.x86_64-linux.ig-story-fetcher;
 
-        packages = rec {
-          inherit (legacyPackages) ig-story-fetcher;
-          default = ig-story-fetcher;
-        };
+      devShells.x86_64-linux.default = pkgs.mkShellNoCC {
+        packages = with pkgs; [
+          (poetry2nix.mkPoetryEnv {
+            projectDir = self;
+            python = pkgs.python311;
+            overrides = p2n-overrides;
+          })
+          poetry
+          python311
+        ];
+      };
 
-        devShells.default = legacyPackages.mkShell {
-          packages = with legacyPackages; [
-            poetry
-            python311
-          ];
-        };
-
-        apps.ig-story-fetcher = flake-utils.lib.mkApp { drv = packages.ig-story-fetcher; };
-      }) // rec {
       nixosModules.ig-story-fetcher = import ./modules/ig-story-fetcher.nix;
-      nixosModules.default = nixosModules.ig-story-fetcher;
+      nixosModules.default = self.nixosModules.ig-story-fetcher;
 
-      overlays.default = localOverlay;
+      overlays.default = _: _: {
+        ig-story-fetcher = self.packages.x86_64-linux.ig-story-fetcher;
+      };
     };
 }
